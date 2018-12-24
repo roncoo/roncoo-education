720cc8300aeba965b3e62289a9215dfd7bd5d479
fengyongwei <297115770@qq.com>
2018-12-24 15:02:58 +0800

    增加feign的调用功能

--- roncoo-education-user/roncoo-education-user-service/src/main/java/com/roncoo/education/UserServiceApplication.java
+++ roncoo-education-user/roncoo-education-user-service/src/main/java/com/roncoo/education/UserServiceApplication.java
@@ -6,6 +6,7 @@ package com.roncoo.education;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import com.spring4all.swagger.EnableSwagger2Doc;
 
@@ -13,6 +14,7 @@ import com.spring4all.swagger.EnableSwagger2Doc;
  * 用户服务
  */
 @EnableSwagger2Doc
+@EnableFeignClients
 @ServletComponentScan
 @SpringCloudApplication
 public class UserServiceApplication {
