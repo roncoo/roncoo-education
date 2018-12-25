a6c4f1b46cc670ef01f9a7d890bd6d358e668193
YZJ <1074760496@qq.com>
2018-12-25 10:39:50 +0800

    修改短信验证工具类

--- roncoo-education-util/src/main/java/com/roncoo/education/util/aliyun/AliyunUtil.java
+++ roncoo-education-util/src/main/java/com/roncoo/education/util/aliyun/AliyunUtil.java
@@ -173,12 +173,12 @@ public final class AliyunUtil {
 		// 必填:待发送手机号。支持以逗号分隔的形式进行批量调用，批量上限为1000个手机号码,批量调用相对于单条调用及时性稍有延迟,验证码类型的短信推荐使用单条调用的方式
 		request.setPhoneNumbers(phone);
 		// 必填:短信签名-可在短信控制台中找到
-		request.setSignName("短信签名");
+		request.setSignName("领课网络");
 		// 必填:短信模板-可在短信控制台中找到
-		request.setTemplateCode("短信模板");
+		request.setTemplateCode("SMS_59740002");
 		// 可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
 		// 友情提示:如果JSON中需要带换行符,请参照标准的JSON协议对换行符的要求,比如短信内容中包含\r\n的情况在JSON中需要表示成\\r\\n,否则会导致JSON在服务端解析失败
-		request.setTemplateParam("");
+		request.setTemplateParam("{\"code\":\"{code}\",\"product\":\"领课开源\"}".replace("{code}", code));
 		// 可选-上行短信扩展码(扩展码字段控制在7位或以下，无特殊需求用户请忽略此字段)
 		// request.setSmsUpExtendCode("90997");
 		// 可选:outId为提供给业务方扩展字段,最终在短信回执消息中将此值带回给调用者
