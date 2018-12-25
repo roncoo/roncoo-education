f25194e5b0aa23af0170ac1e42b8fa8c8bb47ee8
YZJ <1074760496@qq.com>
2018-12-25 09:44:02 +0800

    短信验证码校验

--- roncoo-education-util/src/main/java/com/roncoo/education/util/aliyun/AliyunUtil.java
+++ roncoo-education-util/src/main/java/com/roncoo/education/util/aliyun/AliyunUtil.java
@@ -175,7 +175,7 @@ public final class AliyunUtil {
 		// 必填:短信签名-可在短信控制台中找到
 		request.setSignName("短信签名");
 		// 必填:短信模板-可在短信控制台中找到
-		request.setTemplateCode("短信模板？");
+		request.setTemplateCode("短信模板");
 		// 可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
 		// 友情提示:如果JSON中需要带换行符,请参照标准的JSON协议对换行符的要求,比如短信内容中包含\r\n的情况在JSON中需要表示成\\r\\n,否则会导致JSON在服务端解析失败
 		request.setTemplateParam("");
