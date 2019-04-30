6e629bc8cdb021366dd7f7e381dddca2cb23fbd1
fengyongwei <fengyw@roncoo.com>
2019-04-30 14:50:03 +0800

    修改日志打印的bug

--- roncoo-education-server-config/src/main/java/com/roncoo/education/service/config/ServerConfigApplication.java
+++ roncoo-education-server-config/src/main/java/com/roncoo/education/service/config/ServerConfigApplication.java
@@ -20,6 +20,7 @@ public class ServerConfigApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerConfigApplication.class, args);
+		System.out.println("配置中心(server-config)启动成功，请接着其他服务");
 	}
 
 }
--- roncoo-education-server-eureka/src/main/java/com/roncoo/education/server/eureka/ServerEurekaApplication.java
+++ roncoo-education-server-eureka/src/main/java/com/roncoo/education/server/eureka/ServerEurekaApplication.java
@@ -18,6 +18,7 @@ public class ServerEurekaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServerEurekaApplication.class, args);
+        System.out.println("注册中心(server-eureka)启动成功，请接着启动配置中心(server-config)");
     }
     
 }
