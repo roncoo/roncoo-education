ceecabff4afe05911469427611a431178f638713
keyinghao <keyinghao@roncoo.com>
2019-05-30 16:01:41 +0800

    删除没有接口

--- roncoo-education-boss/src/api/menu.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function getMenu() {
-  return request({
-    url: '/auth/pms/menu/list',
-    method: 'post',
-    data: {}
-  })
-}
--- roncoo-education-boss/src/api/user.js
+++ roncoo-education-boss/src/api/user.js
@@ -1,10 +1,12 @@
 import request from '@/utils/request'
 
-// 根据用户编号获取用户信息、支付配置以及银行账户信息
-export function editUI(data = {}) {
+// 用户密码登录
+export function userLogin(data = {}) {
   return request({
-    url: `/auth/user/userInfo/editUI`,
+    url: `/user/pc/user/login/password`,
     method: 'post',
     data: data
   })
+
+
 }
