46c706d297d8e6e7cd9c07401c7b436d93d6c7fb
keyinghao <keyinghao@roncoo.com>
2019-05-30 14:39:21 +0800

    后台登录

---
+++ roncoo-education-user/roncoo-education-user-service/src/main/java/com/roncoo/education/user/service/biz/pc/PcApiUserLoginBiz.java
@@ -0,0 +1,90 @@
+package com.roncoo.education.user.service.biz.pc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import com.roncoo.education.user.service.common.req.UserLoginPasswordREQ;
+import com.roncoo.education.user.service.common.resq.UserLoginRESQ;
+import com.roncoo.education.user.service.dao.PlatformDao;
+import com.roncoo.education.user.service.dao.UserDao;
+import com.roncoo.education.user.service.dao.UserLogLoginDao;
+import com.roncoo.education.user.service.dao.impl.mapper.entity.Platform;
+import com.roncoo.education.user.service.dao.impl.mapper.entity.User;
+import com.roncoo.education.user.service.dao.impl.mapper.entity.UserLogLogin;
+import com.roncoo.education.util.base.Result;
+import com.roncoo.education.util.enums.LoginStatusEnum;
+import com.roncoo.education.util.enums.StatusIdEnum;
+import com.roncoo.education.util.tools.JWTUtil;
+import com.xiaoleilu.hutool.crypto.DigestUtil;
+
+@Component
+public class PcApiUserLoginBiz {
+
+	@Autowired
+	private PlatformDao platformDao;
+	@Autowired
+	private UserDao userDao;
+	@Autowired
+	private UserLogLoginDao userLogLoginDao;
+
+	@Autowired
+	private RedisTemplate<String, String> redisTemplate;
+
+	public Result<UserLoginRESQ> loginPassword(UserLoginPasswordREQ req) {
+		if (StringUtils.isEmpty(req.getClientId())) {
+			return Result.error("clientId不能为空");
+		}
+		if (StringUtils.isEmpty(req.getMobile())) {
+			return Result.error("手机号不能为空");
+		}
+		if (StringUtils.isEmpty(req.getPassword())) {
+			return Result.error("密码不能为空");
+		}
+		Platform platform = platformDao.getByClientId(req.getClientId());
+		if (null == platform) {
+			return Result.error("该平台不存在");
+		}
+		if (!StatusIdEnum.YES.getCode().equals(platform.getStatusId())) {
+			return Result.error("该平台状态异常，请联系管理员");
+		}
+
+		// 密码错误次数校验
+
+		// 用户校验
+		User user = userDao.getByMobile(req.getMobile());
+		if (null == user) {
+			return Result.error("账号或者密码不正确");
+		}
+		// 密码校验
+		if (!DigestUtil.sha1Hex(user.getMobileSalt() + req.getPassword()).equals(user.getMobilePsw())) {
+			loginLog(user.getUserNo(), req.getClientId(), LoginStatusEnum.FAIL, req.getIp());
+			// 放入缓存，错误次数+1
+			return Result.error("账号或者密码不正确");
+		}
+
+		// 登录日志
+		loginLog(user.getUserNo(), req.getClientId(), LoginStatusEnum.SUCCESS, req.getIp());
+
+		UserLoginRESQ dto = new UserLoginRESQ();
+		dto.setUserNo(user.getUserNo());
+		dto.setMobile(user.getMobile());
+		dto.setToken(JWTUtil.create(user.getUserNo(), JWTUtil.DATE));
+
+		// 登录成功，存入缓存，单点登录使用
+		// redisTemplate.opsForValue().set(dto.getUserNo().toString(), dto.getToken(),
+		// 1, TimeUnit.DAYS);
+
+		return Result.success(dto);
+	}
+
+	private void loginLog(Long userNo, String clientId, LoginStatusEnum status, String ip) {
+		UserLogLogin record = new UserLogLogin();
+		record.setUserNo(userNo);
+		record.setClientId(clientId);
+		record.setLoginStatus(status.getCode());
+		record.setLoginIp(ip);
+		userLogLoginDao.save(record);
+	}
+}
