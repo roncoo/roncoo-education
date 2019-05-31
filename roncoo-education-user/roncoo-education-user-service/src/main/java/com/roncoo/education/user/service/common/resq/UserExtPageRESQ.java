961ac86cf3a304c1339ac46e5d488dcf5d4be666
keyinghao <keyinghao@roncoo.com>
2019-05-31 12:06:43 +0800

    用户创建时间

--- roncoo-education-user/roncoo-education-user-service/src/main/java/com/roncoo/education/user/service/common/resq/UserExtPageRESQ.java
+++ roncoo-education-user/roncoo-education-user-service/src/main/java/com/roncoo/education/user/service/common/resq/UserExtPageRESQ.java
@@ -3,6 +3,7 @@ package com.roncoo.education.user.service.common.resq;
 import java.io.Serializable;
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 
@@ -26,6 +27,7 @@ public class UserExtPageRESQ implements Serializable {
 	 * 创建时间
 	 */
 	@ApiModelProperty(value = "创建时间")
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
 	private Date gmtCreate;
 	/**
 	 * 修改时间
