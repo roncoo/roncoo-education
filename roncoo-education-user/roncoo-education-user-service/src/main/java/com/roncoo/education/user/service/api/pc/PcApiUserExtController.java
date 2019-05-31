16024d5645cfad8ed1259aac8b257d9904fa0421
keyinghao <keyinghao@roncoo.com>
2019-05-31 11:03:46 +0800

    用户管理

--- roncoo-education-user/roncoo-education-user-service/src/main/java/com/roncoo/education/user/service/api/pc/PcApiUserExtController.java
+++ roncoo-education-user/roncoo-education-user-service/src/main/java/com/roncoo/education/user/service/api/pc/PcApiUserExtController.java
@@ -23,7 +23,7 @@ import io.swagger.annotations.ApiOperation;
  *
  */
 @RestController
-@RequestMapping(value = "/user/pc/user/ext")
+@RequestMapping(value = " ")
 public class PcApiUserExtController extends BaseController {
 
 	@Autowired
