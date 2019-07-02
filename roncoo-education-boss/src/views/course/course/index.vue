5687550ffe7550e359e73cb84050b20330ba0398
liaohr <1205148722@qq.com>
2019-07-02 10:39:26 +0800

    解决冲突

--- roncoo-education-boss/src/views/course/course/index.vue
+++ roncoo-education-boss/src/views/course/course/index.vue
@@ -1,45 +1,5 @@
 <template>
   <div class="pad20">
-      <el-table v-loading="ctrl.load" size="medium" :data="list" stripe border style="width: 100%">
-        <el-table-column type="index" label="序号" width="50">
-        </el-table-column>
-        <el-table-column prop="courseName" label="课程名称">
-        </el-table-column>
-        <el-table-column label="课程分类">
-          <template slot-scope="scope">
-          {{scope.row.categoryName1}}/{{scope.row.categoryName2}}/{{scope.row.categoryName3}}
-          </template>
-        </el-table-column>
-        <el-table-column label="是否收费">
-          <template slot-scope="scope">
-            <span :class="textClass(scope.row.isFree)">{{textIsFree[scope.row.isFree]}}</span>
-          </template>
-        </el-table-column>
-        <el-table-column label="原价">
-          <template slot-scope="scope">
-            {{scope.row.courseOriginal.toFixed(2)}}
-          </template>
-        </el-table-column>
-        <el-table-column label="上下架">
-          <template slot-scope="scope">
-            <span :class="textClass(scope.row.isPutaway)">{{textIsPutaway[scope.row.isPutaway]}}</span>
-          </template>
-        </el-table-column>
-        <el-table-column prop="sort" label="排序">
-        </el-table-column>
-        <el-table-column prop="address" width="120" label="操作">
-        </el-table-column>
-      </el-table>
-      <el-pagination
-	    	background
-	    	style="float: right;margin-top: 20px; margin-bottom: 22px"
-	      @size-change="handleSizeChange"
-	      @current-change="handleCurrentChange"
-	      :page-size="page.pageSize"
-	      :page-sizes="[20, 50, 100, 200, 500, 1000]"
-	      layout="total, sizes, prev, pager, next, jumper"
-	      :total="page.totalCount">
-	    </el-pagination>
     <div class="filter-container">
       <el-form :inline="true" size="mini">
         <el-form-item label="课程名称:">
@@ -161,7 +121,6 @@
       :total="page.totalCount">
     </el-pagination>
     <edit :visible="ctrl.editVisible" :formData="formdata" :title="ctrl.dialogTitle" @close-cllback="closeCllback"></edit>
->>>>>>> branch 'dev' of https://github.com/roncoo/roncoo-education.git
   </div>
 </template>
 <script>
