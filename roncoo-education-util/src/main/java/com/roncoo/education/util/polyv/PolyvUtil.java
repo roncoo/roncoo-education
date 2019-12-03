f689773fd6af553688e1ef50ad36894aa456556a
heyh <keyinghao>
2019-12-03 19:54:42 +0800

    播放

--- roncoo-education-util/src/main/java/com/roncoo/education/util/polyv/PolyvUtil.java
+++ roncoo-education-util/src/main/java/com/roncoo/education/util/polyv/PolyvUtil.java
@@ -108,8 +108,6 @@ public final class PolyvUtil {
         map.put("extraParams", "HTML5");
         map.put("viewerId", bo.getUserNo());
         String concated = "extraParams" + map.get("extraParams") + "ts" + map.get("ts") + "userId" + map.get("userId") + "videoId" + map.get("videoId") + "viewerId" + map.get("viewerId") + "viewerIp" + map.get("viewerIp") + "viewerName" + map.get("viewerName");
-        logger.info("保利威视，获取concated接口：result={}", concated);
-        logger.info("保利威视，获取secretkey接口：result={}", secretkey);
         map.put("sign", MD5Util.MD5(secretkey + concated + secretkey).toUpperCase());
         String result = post(SystemUtil.POLYV_GETTOKEN, map);
         logger.info("保利威视，获取token接口：result={}", result);
