043251cc4c4948f1c4b1f1655707c69f41444dff
Quanf <zengqf@aliyun.com>
2021-07-30 12:05:17 +0800

    同步数据

--- roncoo-education-user/roncoo-education-user-service/src/main/java/com/roncoo/education/user/common/bo/UserRegisterBO.java
+++ roncoo-education-user/roncoo-education-user-service/src/main/java/com/roncoo/education/user/common/bo/UserRegisterBO.java
@@ -20,38 +20,41 @@ import java.io.Serializable;
 @Accessors(chain = true)
 public class UserRegisterBO implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * 手机号码
-	 */
-	@ApiModelProperty(value = "手机", required = true)
-	private String mobile;
-	/**
-	 * 登录密码
-	 */
-	@ApiModelProperty(value = "密码", required = true)
-	private String password;
-	/**
-	 * 重复密码
-	 */
-	@ApiModelProperty(value = "重复密码", required = true)
-	private String repassword;
-	/**
-	 * clientId
-	 */
-	@ApiModelProperty(value = "clientId", required = true)
-	private String clientId;
-	/**
-	 * 手机验证码
-	 */
-	@ApiModelProperty(value = "手机验证码", required = true)
-	private String code;
+    /**
+     * 手机号码
+     */
+    @ApiModelProperty(value = "手机", required = true)
+    private String mobile;
+    /**
+     * 登录密码
+     */
+    @ApiModelProperty(value = "密码", required = true)
+    private String password;
+    /**
+     * 重复密码
+     */
+    @ApiModelProperty(value = "重复密码", required = true)
+    private String repassword;
+    /**
+     * clientId
+     */
+    @ApiModelProperty(value = "clientId", required = true)
+    private String clientId;
+    /**
+     * 手机验证码
+     */
+    @ApiModelProperty(value = "手机验证码", required = true)
+    private String code;
 
     @ApiModelProperty(value = "ip地址")
-	private String ip;
+    private String ip;
 
     @ApiModelProperty(value = "注册来源", hidden = true)
     private Integer source = 2;
+
+    @ApiModelProperty(value = "推荐人", hidden = true)
+    private String referralCode = "D92359";
 
 }
