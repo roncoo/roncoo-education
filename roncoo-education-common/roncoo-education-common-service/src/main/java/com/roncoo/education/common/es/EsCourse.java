4ebc75530f165a45672b48388514e3383e41095c
keyh <Keyinghao@123>
2022-07-06 14:07:19 +0800

    课程增加一键导入ES功能

--- roncoo-education-common/roncoo-education-common-service/src/main/java/com/roncoo/education/common/es/EsCourse.java
+++ roncoo-education-common/roncoo-education-common-service/src/main/java/com/roncoo/education/common/es/EsCourse.java
@@ -1,5 +1,7 @@
 package com.roncoo.education.common.es;
 
+import lombok.Data;
+import lombok.experimental.Accessors;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 
@@ -13,6 +15,8 @@ import java.util.Date;
  * @author wuyun
  */
 @Document(indexName = EsCourse.COURSE)
+@Data
+@Accessors(chain = true)
 public class EsCourse implements Serializable {
     public static final String COURSE = "edu_course";
     private static final long serialVersionUID = 1L;
@@ -66,102 +70,6 @@ public class EsCourse implements Serializable {
      * 是否免费(1:免费，0:收费)
      */
     private Integer isFree;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Date getGmtCreate() {
-        return gmtCreate;
-    }
-
-    public void setGmtCreate(Date gmtCreate) {
-        this.gmtCreate = gmtCreate;
-    }
-
-    public Date getGmtModified() {
-        return gmtModified;
-    }
-
-    public void setGmtModified(Date gmtModified) {
-        this.gmtModified = gmtModified;
-    }
-
-    public Integer getSort() {
-        return sort;
-    }
-
-    public void setSort(Integer sort) {
-        this.sort = sort;
-    }
-
-    public String getCourseName() {
-        return courseName;
-    }
-
-    public Long getCourseNo() {
-        return courseNo;
-    }
-
-    public void setCourseNo(Long courseNo) {
-        this.courseNo = courseNo;
-    }
-
-    public void setCourseName(String courseName) {
-        this.courseName = courseName;
-    }
-
-    public Integer getIsFree() {
-        return isFree;
-    }
-
-    public void setIsFree(Integer isFree) {
-        this.isFree = isFree;
-    }
-
-    public Integer getCourseSort() {
-        return courseSort;
-    }
-
-    public void setCourseSort(Integer courseSort) {
-        this.courseSort = courseSort;
-    }
-
-    public String getLecturerName() {
-        return lecturerName;
-    }
-
-    public void setLecturerName(String lecturerName) {
-        this.lecturerName = lecturerName;
-    }
-
-    public String getCourseLogo() {
-        return courseLogo;
-    }
-
-    public void setCourseLogo(String courseLogo) {
-        this.courseLogo = courseLogo;
-    }
-
-    public BigDecimal getCourseOriginal() {
-        return courseOriginal;
-    }
-
-    public void setCourseOriginal(BigDecimal courseOriginal) {
-        this.courseOriginal = courseOriginal;
-    }
-
-    public BigDecimal getCourseDiscount() {
-        return courseDiscount;
-    }
-
-    public void setCourseDiscount(BigDecimal courseDiscount) {
-        this.courseDiscount = courseDiscount;
-    }
 
     @Override
     public String toString() {
