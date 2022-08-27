a3c174329362b3bb91e90ab0502d24d67202f3fa
fengyw <fengyw@roncoo.com>
2022-08-27 17:37:01 +0800

    用户工程调整

--- roncoo-education-system/roncoo-education-system-service/src/main/java/com/roncoo/education/system/service/api/ApiEnumController.java
+++ roncoo-education-system/roncoo-education-system-service/src/main/java/com/roncoo/education/system/service/api/ApiEnumController.java
@@ -4,9 +4,9 @@ import cn.hutool.core.util.StrUtil;
 import com.roncoo.education.common.core.base.Result;
 import com.roncoo.education.common.core.tools.EnumUtil;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -25,7 +25,7 @@ public class ApiEnumController {
 
     @ApiOperation(value = "枚举通用接口", notes = "返回枚举信息")
     @RequestMapping(value = "/view", method = RequestMethod.POST)
-    public Result<ArrayList> getEnumInfo(@RequestParam(name = "enumName") String enumName) {
+    public Result<ArrayList> getEnumInfo(@RequestBody String enumName) {
         if (StrUtil.isBlank(enumName)) {
             return Result.error("请输入要获取的枚举名称");
         }
