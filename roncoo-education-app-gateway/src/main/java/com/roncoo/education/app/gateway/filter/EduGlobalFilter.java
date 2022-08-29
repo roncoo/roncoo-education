ff959927119eb62c09b19f5ba57cb67011458abe
fengyw <fengyw@roncoo.com>
2022-08-29 17:40:19 +0800

    修改

--- roncoo-education-app-gateway/src/main/java/com/roncoo/education/app/gateway/filter/EduGlobalFilter.java
+++ roncoo-education-app-gateway/src/main/java/com/roncoo/education/app/gateway/filter/EduGlobalFilter.java
@@ -43,7 +43,9 @@ public class EduGlobalFilter implements GlobalFilter, Ordered {
             // 登录获取菜单接口
             "/system/admin/sys/menu/user/list",
             // 登录获取权限接口
-            "/system/admin/sys/menu/permission/list"
+            "/system/admin/sys/menu/permission/list",
+            // 登录获取当前用户接口
+            "/system/admin/sys/user/current"
     );
 
     @Autowired
