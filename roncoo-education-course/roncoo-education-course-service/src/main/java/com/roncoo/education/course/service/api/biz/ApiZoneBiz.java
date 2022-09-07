4ee1aa918898f6448ad035143a61f9d742090f7f
fengyw <fengyw@roncoo.com>
2022-09-07 16:45:04 +0800

    修改

--- roncoo-education-course/roncoo-education-course-service/src/main/java/com/roncoo/education/course/service/api/biz/ApiZoneBiz.java
+++ roncoo-education-course/roncoo-education-course-service/src/main/java/com/roncoo/education/course/service/api/biz/ApiZoneBiz.java
@@ -1,5 +1,6 @@
 package com.roncoo.education.course.service.api.biz;
 
+import cn.hutool.core.collection.CollUtil;
 import com.roncoo.education.common.core.base.Result;
 import com.roncoo.education.common.core.enums.StatusIdEnum;
 import com.roncoo.education.common.core.tools.BeanUtil;
@@ -11,10 +12,8 @@ import com.roncoo.education.course.service.api.resp.ApiZoneCourseResp;
 import com.roncoo.education.course.service.api.resp.ApiZoneResp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -32,29 +31,27 @@ public class ApiZoneBiz {
     @Autowired
     private CourseDao courseDao;
 
-    @Cacheable
+    //@Cacheable
     public Result<List<ApiZoneResp>> list() {
         // 获取所有可用专区
         ZoneExample example = new ZoneExample();
         example.createCriteria().andStatusIdEqualTo(StatusIdEnum.YES.getCode());
         example.setOrderByClause(" sort asc, id desc ");
         List<Zone> zoneList = zoneDao.listByExample(example);
-        List<ApiZoneResp> result = new ArrayList<>();
-        for (Zone zone : zoneList) {
-            // 获取专区下的课程
-            List<Long> courseIds = zoneCourse(zone);
-            // 获取对应的课程信息
-            List<Course> courseList = courseDao.listByIds(courseIds);
-            ApiZoneResp resp = BeanUtil.copyProperties(zone, ApiZoneResp.class);
-            resp.setCourseList(BeanUtil.copyProperties(courseList, ApiZoneCourseResp.class));
-            result.add(resp);
+        List<ApiZoneResp> result = BeanUtil.copyProperties(zoneList, ApiZoneResp.class);
+        if (CollUtil.isNotEmpty(result)) {
+            for (ApiZoneResp resp : result) {
+                List<Long> courseIds = zoneCourse(resp.getId());
+                List<Course> courseList = courseDao.listByIds(courseIds);
+                resp.setCourseList(BeanUtil.copyProperties(courseList, ApiZoneCourseResp.class));
+            }
         }
         return Result.success(result);
     }
 
-    private List<Long> zoneCourse(Zone zone) {
+    private List<Long> zoneCourse(Long zoneId) {
         ZoneCourseExample example = new ZoneCourseExample();
-        example.createCriteria().andZoneIdEqualTo(zone.getId());
+        example.createCriteria().andZoneIdEqualTo(zoneId);
         example.setOrderByClause(" sort asc, id desc");
         List<ZoneCourse> result = zoneCourseDao.listByExample(example);
         return result.stream().map(ZoneCourse::getCourseId).collect(Collectors.toList());
