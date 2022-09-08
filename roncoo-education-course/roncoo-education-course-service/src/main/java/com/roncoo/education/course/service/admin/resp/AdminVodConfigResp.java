a5066f36bfe4b9c83d1af1e16cd76472a2105d84
fengyw <fengyw@roncoo.com>
2022-09-08 10:16:20 +0800

    集成保利威最新接口

--- roncoo-education-course/roncoo-education-course-service/src/main/java/com/roncoo/education/course/service/admin/resp/AdminVodConfigResp.java
+++ roncoo-education-course/roncoo-education-course-service/src/main/java/com/roncoo/education/course/service/admin/resp/AdminVodConfigResp.java
@@ -50,5 +50,7 @@ public class AdminVodConfigResp {
     @AllArgsConstructor
     public static class PriConfig {
         // TODO
+        @ApiModelProperty(value = "sign")
+        private String sign;
     }
 }
