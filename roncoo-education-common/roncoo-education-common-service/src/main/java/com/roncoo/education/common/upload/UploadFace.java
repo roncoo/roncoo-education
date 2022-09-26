2648314622a8631bdc326e3fad1974ca0cec87d2
fengyw <fengyw@roncoo.com>
2022-09-26 15:26:23 +0800

    精简代码

--- roncoo-education-common/roncoo-education-common-service/src/main/java/com/roncoo/education/common/upload/UploadFace.java
+++ roncoo-education-common/roncoo-education-common-service/src/main/java/com/roncoo/education/common/upload/UploadFace.java
@@ -2,8 +2,6 @@ package com.roncoo.education.common.upload;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-
 /**
  * 上传功能
  */
@@ -14,5 +12,5 @@ public interface UploadFace {
      *
      * @return 图片url
      */
-    String uploadPic(MultipartFile file, Upload upload) throws IOException;
+    String uploadPic(MultipartFile file, Upload upload);
 }
