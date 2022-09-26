d70e5570c341d1038552109826bd2d3f161f7c12
fengyw <fengyw@roncoo.com>
2022-09-26 15:24:32 +0800

    精简代码

--- roncoo-education-app-gateway/src/main/java/com/roncoo/education/app/gateway/filter/EduGlobalFilter.java
+++ roncoo-education-app-gateway/src/main/java/com/roncoo/education/app/gateway/filter/EduGlobalFilter.java
@@ -33,7 +33,7 @@ public class EduGlobalFilter implements GlobalFilter, Ordered {
      */
     private static final List<String> EXCLUDE_TOKEN_URL = Arrays.asList(
             // 登录接口
-            "/system/admin/login/password",
+            "/system/admin/login/password"
     );
 
     /**
