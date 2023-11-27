90b5c3c61ed95608cb70edfc27d2419fcb0ff748
fengyw <fengyw@roncoo.com>
2023-11-27 15:51:29 +0800

    feat: 异常处理

--- roncoo-education-common/roncoo-education-common-service/src/main/java/com/roncoo/education/common/service/HandlerException.java
+++ roncoo-education-common/roncoo-education-common-service/src/main/java/com/roncoo/education/common/service/HandlerException.java
@@ -5,11 +5,15 @@ import com.roncoo.education.common.core.base.Result;
 import com.roncoo.education.common.core.tools.JSUtil;
 import feign.FeignException;
 import org.springframework.http.HttpStatus;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Map;
+import java.util.StringJoiner;
 
 /**
  * @author wujing
@@ -18,27 +22,40 @@ import java.util.Map;
 public class HandlerException extends BaseController {
 
     @ExceptionHandler({FeignException.class})
-    @ResponseStatus(HttpStatus.OK)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public Result<String> processBizException(FeignException e) {
-        String msg = e.getCause().toString().split("\n|\r\n|\r")[1];
-        log.error(msg);
-        @SuppressWarnings("unchecked")
-        Map<String, Object> m = JSUtil.parseObject(msg, Map.class);
+        String msg = e.getMessage();
+        if (!StringUtils.hasText(msg)) {
+            return Result.error("服务繁忙，请重试");
+        }
+        log.error("FeignException, {}", msg);
+        Map<String, Object> m = JSUtil.parseArray(msg.substring(msg.lastIndexOf("[{")), Map.class).get(0);
         if (null != m.get("message")) {
             return Result.error(m.get("message").toString());
         }
         return Result.error("服务繁忙，请重试");
     }
 
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Result<String> processException(MethodArgumentNotValidException ex) {
+        StringJoiner joiner = new StringJoiner("、");
+        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+            joiner.add(fieldError.getDefaultMessage());
+        }
+        log.error("MethodArgumentNotValidException, {}", joiner);
+        return Result.error(joiner.toString());
+    }
+
     @ExceptionHandler({BaseException.class})
-    @ResponseStatus(HttpStatus.OK)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public Result<String> processException(BaseException e) {
         log.error("BaseException", e);
         return Result.error(e.getMessage());
     }
 
     @ExceptionHandler({Exception.class})
-    @ResponseStatus(HttpStatus.OK)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public Result<String> processException(Exception e) {
         log.error("Exception", e);
         return Result.error("服务繁忙，请重试");
