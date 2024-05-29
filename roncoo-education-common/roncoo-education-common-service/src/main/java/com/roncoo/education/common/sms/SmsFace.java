39b4195603161757ef15683ff395070d3df88fa9
fengyw <fengyw@roncoo.com>
2024-05-29 11:08:20 +0800

    feat: 增加微信登录功能

--- roncoo-education-common/roncoo-education-common-service/src/main/java/com/roncoo/education/common/sms/SmsFace.java
+++ roncoo-education-common/roncoo-education-common-service/src/main/java/com/roncoo/education/common/sms/SmsFace.java
@@ -1,8 +1,18 @@
 package com.roncoo.education.common.sms;
 
 /**
+ * 短信发送
+ *
  * @author fengyw
  */
 public interface SmsFace {
+    /**
+     * 发送短信验证码
+     *
+     * @param mobile 手机
+     * @param code   验证码
+     * @param sms
+     * @return
+     */
     Boolean sendVerCode(String mobile, String code, Sms sms);
 }
