3357cab7965e6337bea4f884d283fab147c88e55
fengyw <fengyw@roncoo.com>
2024-06-06 16:12:14 +0800

    feat: ES增加讲师搜索功能

--- roncoo-education-service/roncoo-education-service-user/src/main/java/com/roncoo/education/user/job/LecturerJob.java
+++ roncoo-education-service/roncoo-education-service-user/src/main/java/com/roncoo/education/user/job/LecturerJob.java
@@ -1,6 +1,6 @@
-package com.roncoo.education.course.job;
+package com.roncoo.education.user.job;
 
-import com.roncoo.education.course.service.admin.biz.AdminCourseBiz;
+import com.roncoo.education.user.service.admin.biz.AdminLecturerBiz;
 import com.xxl.job.core.context.XxlJobHelper;
 import com.xxl.job.core.handler.annotation.XxlJob;
 import lombok.RequiredArgsConstructor;
@@ -10,24 +10,24 @@ import org.springframework.stereotype.Component;
 import javax.validation.constraints.NotNull;
 
 /**
- * 同步课程数据到es
+ * 同步讲师数据到es
  *
  * @author fengyw
  */
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class CourseJob {
+public class LecturerJob {
 
     @NotNull
-    private final AdminCourseBiz adminCourseBiz;
+    private final AdminLecturerBiz adminLecturerBiz;
 
     /**
      * 建议：每天凌晨5点执行一次
      */
     @XxlJob("courseJobHandler")
     public void course() {
-        adminCourseBiz.syncEs();
+        adminLecturerBiz.syncEs();
         XxlJobHelper.handleSuccess("完成");
     }
 
