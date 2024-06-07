b2b185706ffc9c4392c0e99548b3c0f822de12e5
fengyw <fengyw@roncoo.com>
2024-06-07 09:20:23 +0800

    feat: 增加直播课程功能

--- roncoo-education-service/roncoo-education-service-course/src/main/java/com/roncoo/education/course/service/admin/biz/AdminLiveBiz.java
+++ roncoo-education-service/roncoo-education-service-course/src/main/java/com/roncoo/education/course/service/admin/biz/AdminLiveBiz.java
@@ -1,19 +1,19 @@
 package com.roncoo.education.course.service.admin.biz;
 
-import com.roncoo.education.common.service.BaseBiz;
 import com.roncoo.education.common.core.base.Page;
 import com.roncoo.education.common.core.base.PageUtil;
 import com.roncoo.education.common.core.base.Result;
 import com.roncoo.education.common.core.tools.BeanUtil;
+import com.roncoo.education.common.service.BaseBiz;
+import com.roncoo.education.course.dao.LiveDao;
+import com.roncoo.education.course.dao.impl.mapper.entity.Live;
+import com.roncoo.education.course.dao.impl.mapper.entity.LiveExample;
+import com.roncoo.education.course.dao.impl.mapper.entity.LiveExample.Criteria;
 import com.roncoo.education.course.service.admin.req.AdminLiveEditReq;
 import com.roncoo.education.course.service.admin.req.AdminLivePageReq;
 import com.roncoo.education.course.service.admin.req.AdminLiveSaveReq;
 import com.roncoo.education.course.service.admin.resp.AdminLivePageResp;
 import com.roncoo.education.course.service.admin.resp.AdminLiveViewResp;
-import com.roncoo.education.course.dao.LiveDao;
-import com.roncoo.education.course.dao.impl.mapper.entity.Live;
-import com.roncoo.education.course.dao.impl.mapper.entity.LiveExample;
-import com.roncoo.education.course.dao.impl.mapper.entity.LiveExample.Criteria;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
--- roncoo-education-service/roncoo-education-service-course/src/main/java/com/roncoo/education/course/service/admin/resp/AdminLivePageResp.java
+++ roncoo-education-service/roncoo-education-service-course/src/main/java/com/roncoo/education/course/service/admin/resp/AdminLivePageResp.java
@@ -1,69 +1,70 @@
 package com.roncoo.education.course.service.admin.resp;
 
-        import com.fasterxml.jackson.annotation.JsonFormat;
-        import java.time.LocalDateTime;
-        import java.io.Serializable;
-    import io.swagger.annotations.ApiModel;
-    import io.swagger.annotations.ApiModelProperty;
-    import lombok.Data;
-    import lombok.experimental.Accessors;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
 
 /**
-* <p>
-    * ADMIN-直播信息
-    * </p>
-*
-* @author fengyw
-*/
-    @Data
-    @Accessors(chain = true)
-    @ApiModel(description = "ADMIN-直播信息分页")
+ * <p>
+ * ADMIN-直播信息
+ * </p>
+ *
+ * @author fengyw
+ */
+@Data
+@Accessors(chain = true)
+@ApiModel(description = "ADMIN-直播信息分页")
 public class AdminLivePageResp implements Serializable {
 
-private static final long serialVersionUID = 1L;
+        private static final long serialVersionUID = 1L;
 
-                @ApiModelProperty(value = "主键")
+        @ApiModelProperty(value = "主键")
         private Long id;
 
-                @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-                @ApiModelProperty(value = "创建时间")
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        @ApiModelProperty(value = "创建时间")
         private LocalDateTime gmtCreate;
 
-                @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-                @ApiModelProperty(value = "修改时间")
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        @ApiModelProperty(value = "修改时间")
         private LocalDateTime gmtModified;
 
-                @ApiModelProperty(value = "状态(1:正常，0:禁用)")
+        @ApiModelProperty(value = "状态(1:正常，0:禁用)")
         private Integer statusId;
 
-                @ApiModelProperty(value = "排序")
+        @ApiModelProperty(value = "排序")
         private Integer sort;
 
-                @ApiModelProperty(value = "直播模式(1三分屏)")
+        @ApiModelProperty(value = "直播模式(1三分屏)")
         private Integer liveModel;
 
-                @ApiModelProperty(value = "直播延迟(1正常延迟，2无延迟)")
+        @ApiModelProperty(value = "直播延迟(1正常延迟，2无延迟)")
         private Integer liveDelay;
 
-                @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-                @ApiModelProperty(value = "开播时间")
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        @ApiModelProperty(value = "开播时间")
         private LocalDateTime beginTime;
 
-                @ApiModelProperty(value = "开播时长，单位:秒")
+        @ApiModelProperty(value = "开播时长，单位:秒")
         private Integer liveDuration;
 
-                @ApiModelProperty(value = "直播状态(1待开播，2直播中，3已结束，4可回放)")
+        @ApiModelProperty(value = "直播状态(1待开播，2直播中，3已结束，4可回放)")
         private Integer liveStatus;
 
-                @ApiModelProperty(value = "回放保存(1保存，2丢弃)")
+        @ApiModelProperty(value = "回放保存(1保存，2丢弃)")
         private Integer playbackSave;
 
-                @ApiModelProperty(value = "直播平台")
+        @ApiModelProperty(value = "直播平台")
         private Integer livePlatform;
 
-                @ApiModelProperty(value = "频道ID(记录开播频道)")
+        @ApiModelProperty(value = "频道ID(记录开播频道)")
         private String channelId;
 
-                @ApiModelProperty(value = "资源ID(记录回放视频)")
+        @ApiModelProperty(value = "资源ID(记录回放视频)")
         private Long resourceId;
 }
