b93a9893d2c15e5760127489bdbfa638e91fcabf
fengyw <fengyw@roncoo.com>
2025-03-11 08:41:18 +0800

    feat: 修改验证码生成规则，更方便识别

--- roncoo-education-service/roncoo-education-service-system/src/main/java/com/roncoo/education/system/service/api/ApiCommonController.java
+++ roncoo-education-service/roncoo-education-service-system/src/main/java/com/roncoo/education/system/service/api/ApiCommonController.java
@@ -2,6 +2,8 @@ package com.roncoo.education.system.service.api;
 
 import cn.hutool.captcha.CaptchaUtil;
 import cn.hutool.captcha.CircleCaptcha;
+import cn.hutool.captcha.generator.RandomGenerator;
+import cn.hutool.core.util.RandomUtil;
 import com.roncoo.education.common.cache.CacheRedis;
 import com.roncoo.education.common.core.base.Result;
 import com.roncoo.education.common.core.tools.Constants;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -39,11 +42,11 @@ public class ApiCommonController {
 
     @ApiOperation(value = "枚举通用接口", notes = "返回枚举信息")
     @RequestMapping(value = "/enum", method = RequestMethod.POST)
-    public Result<ArrayList> getEnumInfo(@RequestBody ApiEnumReq req) {
-        String className = new StringBuffer(DEFAULT_PREFIX).append(req.getEnumName()).toString();
+    public Result<ArrayList<?>> getEnumInfo(@RequestBody ApiEnumReq req) {
+        String className = DEFAULT_PREFIX + req.getEnumName();
         try {
-            Class clazz = Class.forName(className);
-            return Result.success(new ArrayList<>(EnumUtil.toList(clazz, DEFAULT_ENUM_NAME)));
+            Class<?> clazz = Class.forName(className);
+            return Result.success(new ArrayList<>(Objects.requireNonNull(EnumUtil.toList(clazz, DEFAULT_ENUM_NAME))));
         } catch (ClassNotFoundException e) {
             log.error("获取枚举失败, className={}", className, e);
             return Result.error("获取枚举失败");
@@ -53,7 +56,7 @@ public class ApiCommonController {
     @ApiOperation(value = "获取图片验证码", notes = "获取图片验证码")
     @GetMapping(value = "/code")
     public Result<ApiVerCodeResp> getVerCode() {
-        CircleCaptcha captcha = CaptchaUtil.createCircleCaptcha(200, 100, 4, 18);
+        CircleCaptcha captcha = CaptchaUtil.createCircleCaptcha(200, 100, new RandomGenerator(RandomUtil.BASE_NUMBER, 4), 18);
         ApiVerCodeResp resp = new ApiVerCodeResp(String.valueOf(IdWorker.getId()), captcha.getImageBase64Data());
         // 存入缓存，过期时间为5分钟
         cacheRedis.set(Constants.RedisPre.VER_CODE + resp.getVerToken(), captcha.getCode().toLowerCase(), 5, TimeUnit.MINUTES);
